9af6692205d66e35e556d9572544e1de5a53bf47
Gradey Cullins <gradeycullins@gmail.com>
2021-01-08 13:53:52 -0700

    Add envrc and DB bootstrap script
    Also removed sql script to create table, instead is done in script

---
+++ start-db.sh
@@ -0,0 +1,10 @@
+#/usr/bin/env bash
+docker run --name purity-pg \
+    -p ${PURITY_DB_PORT}:5432 \
+    -e POSTGRES_DB="${PURITY_DB_NAME}" \
+    -e POSTGRES_PASSWORD="${PURITY_DB_PASS}" \
+    -v /pg-docker-data:/var/lib/postgresql/data \
+    -v ${PROJECT_ROOT}/build:/docker-entrypoint-initdb.d \
+    --rm \
+    --detach \
+    postgres
