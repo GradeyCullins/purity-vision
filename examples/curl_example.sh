b2ec61187e7c637e0ba7c2e8221fa4c0084de587
Gradey Cullins <gradeycullins@gmail.com>
2021-01-15 13:14:36 -0700

    Add githooks directory
    - add setup script, add more to it later
    - close #20

--- examples/curl_example.sh
+++ examples/curl_example.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 curl -i ${API_BASE_URL}/filter \
-    -d '{"imgUriList": ["https://i.imgur.com/gcWltJm.jpg", "https://previews.123rf.com/images/valio84sl/valio84sl1311/valio84sl131100006/23554524-autumn-landscape-orange-trre.jpg", "https://i.imgur.com/Vdob7RN.jpg"]}'
+    -d '{"imgUriList": ["https://previews.123rf.com/images/valio84sl/valio84sl1311/valio84sl131100006/23554524-autumn-landscape-orange-trre.jpg"]}'
