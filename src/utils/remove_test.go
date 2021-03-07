a1f74580ee0ff3d9b5784998d95c4358651e33e9
Gradey Cullins <gradeycullins@gmail.com>
2021-03-07 15:12:33 -0700

    Close #37
    - fix the bug
    - improve tests

--- src/utils/remove_test.go
+++ src/utils/remove_test.go
@@ -1,23 +1,56 @@
 package utils
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestStringSliceRemove(t *testing.T) {
+	var err error
 	arr := []string{"hello", "world", "fubar"}
-	expected := []string{"hello", "world"}
+	size := len(arr)
+	removed := 1
 
-	res, err := StringSliceRemove(arr, 2)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	for i := range res {
-		if res[i] != expected[i] {
-			t.Fatalf("found %s but expected %s", res[i], expected[i])
+	for i := 0; i < size; i++ {
+		arr, err = StringSliceRemove(arr, 0)
+		if err != nil {
+			t.Fatal(err)
 		}
+		if len(arr) != size-removed {
+			t.Fatalf("result array should have size: %d\n", size-removed)
+		}
+		removed += 1
 	}
 
-	if _, err = StringSliceRemove(arr, 100); err == nil {
+	if len(arr) != 0 {
+		t.Fatalf("result array should be empty")
+	}
+
+	if _, err := StringSliceRemove(arr, 100); err == nil {
 		t.Fatal("an error should have been thrown")
 	}
+
+	arr = []string{"hello", "world", "fubar"}
+
+	// Do a few arbitrary removes.
+	if arr, err = StringSliceRemove(arr, 1); err != nil {
+		t.Fatal(err)
+	}
+	if len(arr) != 2 {
+		t.Fatalf("result array should have two elements")
+	}
+
+	if arr, err = StringSliceRemove(arr, 1); err != nil {
+		t.Fatal(err)
+	}
+	if len(arr) != 1 {
+		t.Fatalf("result array should have one element")
+	}
+
+	if arr, err = StringSliceRemove(arr, 0); err != nil {
+		t.Fatal(err)
+	}
+	if len(arr) != 0 {
+		t.Fatalf("result array should be empty")
+	}
+
 }
